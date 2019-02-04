011aac614bc06687fd68df3d95a99c618ca50e73
Ivan <devskyfly@gmail.com>
2019-02-04 10:25:30 +0300

    Add cors behavior.

--- controllers/rest/CommonController.php
+++ controllers/rest/CommonController.php
@@ -2,12 +2,27 @@
 namespace devskyfly\yiiModuleIitUc\controllers\rest;
 
 use Yii;
+use yii\filters\Cors;
+use yii\helpers\ArrayHelper;
 use yii\web\Response;
 use yii\web\Controller;
 
 abstract class CommonController extends Controller
 {
     public $mode_list=[];
+    
+    public function behaviors()
+    {
+        return ArrayHelper::merge([
+            [
+                'class' => Cors::className(),
+                'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['*'],
+                ],
+            ],
+        ], parent::behaviors());
+    }
     
     public function init()
     {
