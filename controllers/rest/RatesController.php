475c87528882929897e502287dd55e73fd8aced9
Ivan <devskyfly@gmail.com>
2019-04-22 16:23:12 +0300

    Fix api.

--- controllers/rest/RatesController.php
+++ controllers/rest/RatesController.php
@@ -61,88 +61,105 @@ class RatesController extends CommonController
         $this->asJson($chain);
     }
     
-    public function actionIndex(array $ids){
+    public function actionIndex(array $ids=null){
         try{
-            $result=[];
-            $models=[];
-            $ratesPackages=[];
-            
-            foreach ($ids as $id){
-                $models[]=RatesManager::getBySlxId($id);
-            }
-            
-            $modelCnt=Arr::getSize($models);
-            for($mdlItr=0;$mdlItr<$modelCnt;$mdlItr++){
-                $ratePackage=RatePackageManager::getByRate($models[$mdlItr]);
-                if(!Vrbl::isNull($ratePackage)){
-                    unset($models[$mdlItr]);
-                    $parentRate=Rate::getById($ratePackage->_parent_rate__id);
-                    if(Vrbl::isNull($parentRate)){
-                        throw new \RuntimeException('Parameter $parentRate is null.');
-                    }
-                    $models[]=$parentRate;
-                    $ratesPackages[]=$ratePackage;
+            if(!Vrbl::isNull($ids)){
+                $result=[];
+                $models=[];
+                $ratesPackages=[];
+                
+                foreach ($ids as $id){
+                    $models[]=RatesManager::getBySlxId($id);
                 }
-            }
-            
-            array_unique($models);
-            
-            $promoList = new PromoList();
-            $bindsList = new BindsList();
-    
-            $chain=RatesManager::getMultiChain($models, $promoList, $bindsList);
-            
-            foreach ($chain as $item){
                 
-                $powersPackages=RateToPowerPackageBinder::getSlaveItems($item->id);
-                $powers_packages_ids=[];
-                $rates_packages_ids=[];
-                
-                foreach ($powersPackages as $powerPackage){
-                    if($powerPackage->active==PowerPackage::ACTIVE){
-                        $powers_packages_ids[]=$package->id;
+                $modelCnt=Arr::getSize($models);
+                for($mdlItr=0;$mdlItr<$modelCnt;$mdlItr++){
+                    $ratePackage=RatePackageManager::getByRate($models[$mdlItr]);
+                    if(!Vrbl::isNull($ratePackage)){
+                        unset($models[$mdlItr]);
+                        $parentRate=Rate::getById($ratePackage->_parent_rate__id);
+                        if(Vrbl::isNull($parentRate)){
+                            throw new \RuntimeException('Parameter $parentRate is null.');
+                        }
+                        $models[]=$parentRate;
+                        $ratesPackages[]=$ratePackage;
                     }
                 }
                 
-                foreach ($ratesPackages as $ratePackage){
-                    if($ratePackage['_parent_rate__id']==$item->id){
-                        $packageRates=RatePackageManager::getRates($ratePackage);
-                        foreach ($packageRates as $packageRate){
-                            $rates_packages_ids[]=$packageRate->id;
+                array_unique($models);
+                
+                $promoList = new PromoList();
+                $bindsList = new BindsList();
+        
+                $chain=RatesManager::getMultiChain($models, $promoList, $bindsList);
+                
+                foreach ($chain as $item){
+                    
+                    $powersPackages=RateToPowerPackageBinder::getSlaveItems($item->id);
+                    $powers_packages_ids=[];
+                    $rates_packages_ids=[];
+                    
+                    foreach ($powersPackages as $powerPackage){
+                        if($powerPackage->active==PowerPackage::ACTIVE){
+                            $powers_packages_ids[]=$powerPackage->id;
                         }
                     }
-                }
-                
-                $rates_packages_ids=array_unique($rates_packages_ids);
-                
-                $stock=null;
-                if(!Vrbl::isEmpty($item->_stock__id)){
-                    $stock=Stock::find()
-                    ->where([
-                        'active'=>Stock::ACTIVE,
-                        'id'=>$item->_stock__id  
-                    ])
-                    ->one();
                     
-                    if(Vrbl::isEmpty($stock)){
-                        throw new \RuntimeException('Parameter $stock is empty.');
+                    foreach ($ratesPackages as $ratePackage){
+                        if($ratePackage['_parent_rate__id']==$item->id){
+                            $packageRates=RatePackageManager::getRates($ratePackage);
+                            foreach ($packageRates as $packageRate){
+                                $rates_packages_ids[]=$packageRate->id;
+                            }
+                        }
                     }
+                    
+                    $rates_packages_ids=array_unique($rates_packages_ids);
+                    
+                    $stock=null;
+                    if(!Vrbl::isEmpty($item->_stock__id)){
+                        $stock=Stock::find()
+                        ->where([
+                            'active'=>Stock::ACTIVE,
+                            'id'=>$item->_stock__id  
+                        ])
+                        ->one();
+                        
+                        if(Vrbl::isEmpty($stock)){
+                            throw new \RuntimeException('Parameter $stock is empty.');
+                        }
+                    }
+                    
+                    
+                    
+                    $result[]=[
+                        "id"=>$item->id,
+                        "name"=>$item->name,
+                        "slx_id"=>$item->slx_id,
+                        "price"=>Nmbr::toDoubleStrict($item->price),
+                        "powers_packages"=>$powers_packages_ids,
+                        "rates_packages"=>$rates_packages_ids,
+                        "required_powers"=>[],
+                        "stock_id"=>Vrbl::isNull($stock)?'':$stock->stock,
+                        "required_license"=>$item->flag_for_license=='Y'?true:false
+                    ];
+                }    
+                
+            }else{
+                $models=[];
+ 
+                $rates=Rate::find()->where(['active'=>'Y'])->all();
+                foreach ($rates as $rate){
+                    
+                    $result[]=[
+                        "id"=>$rate->id,
+                        "name"=>$rate->name,
+                        "slx_id"=>$rate->slx_id,
+                        "price"=>Nmbr::toDoubleStrict($rate->price),
+                        //"stock_id"=>Vrbl::isNull($stock)?'':$stock->stock,
+                    ];
                 }
-                
-                
-                
-                $result[]=[
-                    "id"=>$item->id,
-                    "name"=>$item->name,
-                    "slx_id"=>$item->slx_id,
-                    "price"=>Nmbr::toDoubleStrict($item->price),
-                    "powers_packages"=>$powers_packages_ids,
-                    "rates_packages"=>$rates_packages_ids,
-                    "required_powers"=>[],
-                    "stock_id"=>Vrbl::isNull($stock)?'':$stock->stock,
-                    "required_license"=>$item->flag_for_license=='Y'?true:false
-                ];
-            }    
+            }
             $this->asJson($result);
         }catch(\Exception $e)
         {
