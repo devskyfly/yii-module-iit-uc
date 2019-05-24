25a0e8a1cea77d5f6d372dae5ab42acb10f5f8dc
Ivan <devskyfly@gmail.com>
2019-05-24 09:22:26 +0300

    Fix SiteController.

--- controllers/SitesController.php
+++ controllers/SitesController.php
@@ -64,7 +64,7 @@ class SitesController extends AbstractContentPanelController
                     .$form->field($item,'create_date_time')
                     .$form->field($item,'change_date_time')
                     .$form->field($item,'sort')
-                    .$form->field($item,'active')->checkbox(['value'=>'Y','uncheckValue'=>'N','checked'=>$item->active=='Y'?true:false])
+                    .$form->field($item,'active')->checkbox(['value'=>'Y','uncheck'=>'N','checked'=>$item->active=='Y'?true:false])
                     .$form->field($item,'url')
                     .$form->field($item,'comment')->textarea(["rows"=>5])
                     .$form->field($item,'tooltip')->textarea(["rows"=>5])
