3c72a6ef551b0ca97095641e689ed000c5a1c993
Ivan <devskyfly@gmail.com>
2019-05-30 16:07:32 +0300

    Add migrations.

---
+++ migrations/m190530_123324_alter_service_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+use devskyfly\yiiModuleIitUc\models\service\Service;
+
+/**
+ * Class m190530_123324_alter_service_table
+ */
+class m190530_123324_alter_service_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $sql = "ALTER TABLE ".Service::tableName()." ADD COLUMN flag_is_fast_release ENUM('Y','N') DEFAULT 'N';";
+        $sql = "ALTER TABLE ".Service::tableName()." ADD COLUMN flag_for_license ENUM('Y','N') DEFAULT 'N';";
+        $this->execute($sql);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190530_123324_alter_service_table cannot be reverted.\n";
+        $this->dropColumn(Service::tableName(),'flag_is_fast_release');
+        $this->dropColumn(Service::tableName(),'flag_for_license');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190530_123324_alter_service_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
